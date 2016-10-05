0490e0a087b9d67fc9e19fa6aa5c0768ef33337f
Chteufleur <chteufleur@kingpenguin.tk>
2016-10-05 21:26:42 +0200

    Add support of Remote Roster Manager (XEP-0321).

---
+++ src/xmpp/remoteRosterManager.go
@@ -0,0 +1,21 @@
+package xmpp
+
+import (
+	"encoding/xml"
+)
+
+const (
+	NSRemoteRosterManager = "urn:xmpp:tmp:roster-management:0"
+
+  RemoteRosterManagerTypeRequest  = "request"
+  RemoteRosterManagerTypeAllowed  = "allowed"
+  RemoteRosterManagerTypeRejected = "rejected"
+)
+
+// XEP-0321: Remote Roster Manager
+
+type RemoteRosterManagerQuery struct {
+	XMLName xml.Name `xml:"urn:xmpp:tmp:roster-management:0 query"`
+	Reason  string   `xml:"reason,attr,omitempty"`
+	Type    string   `xml:"type,attr"`
+}
